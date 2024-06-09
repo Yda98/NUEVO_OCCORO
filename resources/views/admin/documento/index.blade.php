7c88a7819d3407a5ccdbd15adba85a87d39b13ec
Yda98 <2019141030@unh.edu.pe>
2024-06-09 10:25:04 -0500

    tercer cambio

--- resources/views/admin/documento/index.blade.php
+++ resources/views/admin/documento/index.blade.php
@@ -17,7 +17,7 @@
                             <tr>
                                 <th scope="col"
                                     class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                    YDA</th>
+                                    ID</th>
                                 <th scope="col"
                                     class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                                     Nombre</th>
